a08fad4ac0250e9d5a42fe1159bdd76c4bfabb27
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-07-05 17:17:25 -0400

    features: settings setup the project

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const PageScreen = () => {
+    return (
+        <View>
+            <Text>Instagram Clone</Text>
+        </View>
+    );
+};
+
+export default PageScreen;
